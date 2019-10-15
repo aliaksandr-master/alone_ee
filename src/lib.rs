813d2f7dc178d53eca1840311081c139f022459b
alxe.master <alxe.master@gmail.com>
2019-10-15 17:55:37 +0300

    remove mutable action in emit

--- src/lib.rs
+++ src/lib.rs
@@ -1,46 +1,39 @@
 use std::sync::{Arc, RwLock};
 
 pub struct Listener<T> {
-    _id: usize,
-    pub trigger: Box<dyn FnMut(Arc<T>)>,
+    id: usize,
+    pub handler: Box<dyn FnMut(Arc<T>)>,
 }
 
 pub struct EventEmitter<T> {
-    _next_lstnr_id: usize,
-    _fired_times: usize,
+    next_listener_id: usize,
     pub listeners: Arc<RwLock<Vec<Listener<T>>>>,
 }
 
 impl<T> EventEmitter<T> {
     pub fn new() -> Self {
         EventEmitter {
-            _next_lstnr_id: 0,
-            _fired_times: 0,
+            next_listener_id: 0,
             listeners: Arc::new(RwLock::new(Vec::new())),
         }
     }
 
-    pub fn fired_times(&self) -> usize {
-        self._fired_times
+    pub fn len(&self) -> usize {
+        self.listeners.read().expect("not poisoned").len()
     }
 
     pub fn reset(&mut self) {
-        self._fired_times = 0;
-        self._next_lstnr_id = 0;
         *(self.listeners.write().expect("not poisoned")) = Vec::new();
     }
 
-    pub fn on(&mut self, listener: Box<dyn FnMut(Arc<T>)>) -> impl FnOnce() {
-        let id = self._next_lstnr_id;
-        self._next_lstnr_id += 1;
+    pub fn on(&mut self, handler: Box<dyn FnMut(Arc<T>)>) -> impl FnOnce() {
+        let id = self.next_listener_id;
+        self.next_listener_id += 1;
 
         self.listeners
             .write()
             .expect("not poisoned")
-            .push(Listener {
-                _id: id,
-                trigger: listener,
-            });
+            .push(Listener { id, handler });
 
         let listeners = self.listeners.clone();
 
@@ -54,14 +47,13 @@ impl<T> EventEmitter<T> {
             listeners
                 .write()
                 .expect("not poisoned")
-                .retain(|listener| listener._id != id);
+                .retain(|listener| listener.id != id);
         }
     }
 
-    pub fn emit(&mut self, value: Arc<T>) -> Arc<T> {
-        self._fired_times += 1;
+    pub fn emit(&self, value: Arc<T>) -> Arc<T> {
         for lst in self.listeners.write().expect("not poisoned").iter_mut() {
-            (lst.trigger)(value.clone());
+            (lst.handler)(value.clone());
         }
         value
     }
@@ -95,9 +87,17 @@ mod tests {
             txt: "hello",
         });
 
+        assert!(fired_ev.read().expect("not poisoned").is_none());
+
         ee.emit(ev1.clone());
 
+        assert!(fired_ev.read().expect("not poisoned").is_some());
+
+        assert_eq!(ee.len(), 1);
+
         rm();
+
+        assert_eq!(ee.len(), 0);
 
         let ev2 = Arc::new(SomeEvent {
             ev: 333,
@@ -109,11 +109,21 @@ mod tests {
         //        let tmp_ev1 = ev1.clone();
         //        let tmp_evF = fired_ev.into_inner().unwrap().unwrap().clone();
 
-        assert_eq!(ee.fired_times(), 2);
         //        assert!(Arc::ptr_eq(&tmp_ev1, &tmp_evF));
+
+        let fired_ev_clone = fired_ev.clone();
+
+        let rm = ee.on(Box::new(move |ev| {
+            let mut mut_fired_ev = fired_ev_clone.write().unwrap();
+            *mut_fired_ev = Option::Some(ev.clone())
+        }));
+
+        assert_eq!(ee.len(), 1);
 
         ee.reset();
 
-        assert_eq!(ee.fired_times(), 0);
+        rm();
+
+        assert_eq!(ee.len(), 0);
     }
 }
