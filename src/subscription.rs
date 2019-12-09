159d8fd7a4da77474410d8456e1d6aa748ef0cc8
alxe.master <alxe.master@gmail.com>
2019-12-10 01:10:01 +0300

    add subscription display/debug for subscription

--- src/subscription.rs
+++ src/subscription.rs
@@ -1,8 +1,22 @@
 use crate::listener::Listener;
+use std::fmt;
+use std::fmt::{Error, Formatter};
 use std::sync::{RwLock, Weak};
 
 pub struct Subscription<TEvent> {
     listener: Option<Weak<RwLock<Listener<TEvent>>>>,
+}
+
+impl<TEvent> fmt::Debug for Subscription<TEvent> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "Subscription<{}>", if self.listener.is_some() { "active" } else { "inactive" })
+    }
+}
+
+impl<TEvent> fmt::Display for Subscription<TEvent> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "Subscription<{}>", if self.listener.is_some() { "active" } else { "inactive" })
+    }
 }
 
 impl<TEvent> Subscription<TEvent> {
